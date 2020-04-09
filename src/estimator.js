093f4df165f3088abf4ff08ce1717e680d667ded
daaef <realafe21@gmail.com>
2020-04-09 20:24:46 +0100

    assessment 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,24 @@
-const covid19ImpactEstimator = (data) => data;
+function currentlyInfectedPeople(reportedCases, times) {
+  return reportedCases * times;
+}
+
+function infectionsRequested(currentlyInfected, period) {
+  return currentlyInfected * 2 ** (period / 3);
+}
+
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  return {
+    data: input,
+    impact: {
+      currentlyInfected: currentlyInfectedPeople(data.reportedCases, 10),
+      infectionsByRequestedTime: infectionsRequested(this.currentlyInfected, data.timeToElapse)
+    },
+    severeImpact: {
+      currentlyInfected: currentlyInfectedPeople(data.reportedCases, 50),
+      infectionsByRequestedTime: infectionsRequested(this.currentlyInfected, data.timeToElapse)
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
