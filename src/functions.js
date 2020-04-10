6becbbea9348e4cf902b56cf905e525939525d87
daaef <realafe21@gmail.com>
2020-04-10 13:55:06 +0100

    back to the math function pain 3

--- src/functions.js
+++ src/functions.js
@@ -35,20 +35,20 @@ export function dollarsInFlightCost(
 export function deriveImpact({ region, ...content }, multiplier = 1) {
   const days = getNumOfDays(content);
 
-  const infectionFactor = Math.trunc(days / 3);
+  const infectionFactor = Math.floor(days / 3);
 
   const currentlyInfected = currentlyInfectedPeople(content.reportedCases, multiplier);
 
   const infectionsByRequestedTime = currentlyInfected * (2 ** infectionFactor);
 
-  const severeCasesByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.15);
+  const severeCasesByRequestedTime = Math.floor(infectionsByRequestedTime * 0.15);
 
-  const hospitalBedsByRequestedTime = Math.trunc(content.totalHospitalBeds * 0.35)
+  const hospitalBedsByRequestedTime = Math.ceil(content.totalHospitalBeds * 0.35)
     - severeCasesByRequestedTime;
 
-  const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
+  const casesForICUByRequestedTime = Math.floor(infectionsByRequestedTime * 0.05);
 
-  const casesForVentilatorsByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
+  const casesForVentilatorsByRequestedTime = Math.floor(infectionsByRequestedTime * 0.02);
 
   const dollarsInFlight = dollarsInFlightCost(
     infectionsByRequestedTime, days,
