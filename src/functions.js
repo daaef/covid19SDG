c3900c3bbf82f9758bbde79a630b27469fe6f05b
daaef <realafe21@gmail.com>
2020-04-11 12:42:36 +0100

    challenge with Math functions 2

--- src/functions.js
+++ src/functions.js
@@ -43,7 +43,7 @@ export function deriveImpact({ region, ...content }, multiplier = 1) {
 
   const severeCasesByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.15);
 
-  const hospitalBedsByRequestedTime = Math.round((content.totalHospitalBeds * 0.35)
+  const hospitalBedsByRequestedTime = Math.floor((content.totalHospitalBeds * 0.35)
     - severeCasesByRequestedTime);
 
   const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
